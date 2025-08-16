3312e5153512e32bb1477993810f49e73dfb68ec
James Upson <upsonjames1@gmail.com>
2025-08-16 05:39:39 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient";
 import speakeasy from "speakeasy";
-import { supabase } from "../supabaseClient";
 
 export default function Login() {
   const [email, setEmail] = useState("");
